2800dfa13c5b9458ead0497265476b6115d78997
Eric Firing <efiring@hawaii.edu>
2017-03-29 09:09:20 -1000

    Factor some docstring functions out into their own module.

--- make_wrapped_ufuncs.py
+++ make_wrapped_ufuncs.py
@@ -10,6 +10,9 @@ from pycurrents.system import Bunch
 from matlab_parser import get_complete_sigdict, get_helpdict
 from c_header_parser import get_signatures, parse_signatures
 from docstring_parts import parameters
+from docstring_utils import (paragraphs,
+                             fix_outputs_doc,
+                             docstring_from_sections)
 
 wrapmod = 'gsw/_wrapped_ufuncs.py'
 
@@ -115,71 +118,6 @@ def get_outname_set():
         if args is not None:
             argset.update(args)
     return argset
-
-def paragraphs(linelist):
-    plist = []
-    newlinelist = []
-    for line in linelist:
-        line = line.strip()
-        if line:
-            newlinelist.append(line)
-        elif newlinelist:
-            plist.append(newlinelist)
-            newlinelist = []
-    if newlinelist:
-        plist.append(newlinelist)
-    return plist
-
-def fix_outputs_doc(lines):
-    for i, line in enumerate(list(lines)):
-        match = re.search(r'(.*)\[(.*)\]', line)
-        if match is not None:
-            units = match.group(2).strip()
-            remainder = match.group(1).strip()
-            lines[i] = remainder
-        else:
-            lines[i] = line.strip()
-
-    outname, remainder = lines[0].split('=')
-    outlines = ['%s : array-like, %s' % (outname.strip(), units)]
-    # FIXME: we need to assemble the rest into a single line
-    # and then break it into lines of appropriate length.
-    for line in lines[1:]:
-        if line:
-            outlines.append('    ' + line)
-    return outlines
-
-def docstring_from_sections(sections):
-    """
-    sections is a dictionary containing numpydoc text sections,
-    without their headers.  Everything above the Parameters is
-    considered Head; it does not have to follow the standard of
-    having a single line "short summary", etc.  Each section
-    must be a list of lines without newlines, and with
-    indentation only relative to the edge of the docstring.
-
-    We start with only 3 sections: Head, Parameters, and Returns;
-    others may be added as options later.
-
-    """
-    doclines = ['']
-    doclines.extend(sections['Head'])
-    doclines.append('')
-    doclines.append('Parameters')
-    doclines.append('----------')
-    doclines.extend(sections['Parameters'])
-    doclines.append('')
-    doclines.append('Returns')
-    doclines.append('-------')
-    doclines.extend(sections['Returns'])
-
-    for i, line in enumerate(list(doclines)):
-        if line:
-            doclines[i] = '    %s\n' % line
-        else:
-            doclines[i] = '\n'
-
-    return ''.join(doclines)
 
 
 def uf_wrapper(ufname):
