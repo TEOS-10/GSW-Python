cf885bd82f60f74bf3b3a0138bc793ad77076f77
Eric Firing <efiring@hawaii.edu>
2017-04-23 13:00:56 -1000

    Add __all__, p_from_z to conversions; list common functions in docstring

--- gsw/conversions.py
+++ gsw/conversions.py
@@ -1,7 +1,47 @@
 """
 Conversions involving temperature, salinity, entropy, pressure,
 and height.
+
+Those most commonly used probably include:
+
+- :func:`gsw.CT_from_t`
+- :func:`gsw.SA_from_SP`
+- :func:`gsw.SP_from_C`
+- :func:`gsw.p_from_z`
+- :func:`gsw.z_from_p`
+
 """
+
+__all__ = ['t90_from_t68',
+            'adiabatic_lapse_rate_from_CT',
+            'C_from_SP',
+            'CT_from_enthalpy',
+            'CT_from_entropy',
+            'CT_from_pt',
+            'CT_from_rho',
+            'CT_from_t',
+            'deltaSA_from_SP',
+            'entropy_from_pt',
+            'entropy_from_t',
+            'pt0_from_t',
+            'pt_from_CT',
+            'pt_from_entropy',
+            'pt_from_t',
+            'SA_from_rho',
+            'SA_from_SP',
+            'SA_from_Sstar',
+            'SP_from_C',
+            'SP_from_SA',
+            'SP_from_SK',
+            'SP_from_SR',
+            'SP_from_Sstar',
+            'SR_from_SP',
+            'Sstar_from_SA',
+            'Sstar_from_SP',
+            't_from_CT',
+            'p_from_z',
+            'z_from_p',
+            ]
 
 from ._utilities import match_args_return
 
@@ -32,6 +72,7 @@ SR_from_SP,
 Sstar_from_SA,
 Sstar_from_SP,
 t_from_CT,
+p_from_z,
 z_from_p,
 )
 
