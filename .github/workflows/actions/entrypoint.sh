2db6d1eb613a7896b17ae1f63145d7b8549ec5bd
Filipe Fernandes <ocefpaf@gmail.com>
2022-02-24 18:45:09 -0300

    remove cython and pep517

--- .github/workflows/actions/entrypoint.sh
+++ .github/workflows/actions/entrypoint.sh
@@ -6,7 +6,7 @@
 PYTHONS=("cp38-cp38" "cp39-cp39" "cp310-cp310")
 
 for PYTHON in ${PYTHONS[@]}; do
-    /opt/python/${PYTHON}/bin/pip install --upgrade pip wheel setuptools setuptools_scm pep517 twine auditwheel
+    /opt/python/${PYTHON}/bin/pip install --upgrade pip wheel setuptools setuptools_scm build twine auditwheel
     /opt/python/${PYTHON}/bin/pip install oldest-supported-numpy
     /opt/python/${PYTHON}/bin/python -m build --sdist --wheel . --outdir /github/workspace/wheelhouse/
 done
