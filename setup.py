88a3c7617518d785d239b0087b4309bd629044c2
Filipe Fernandes <ocefpaf@gmail.com>
2022-11-15 14:48:35 -0300

    add name to setup.py so we can get GH dep graph

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
-'''
+"""
 Minimal setup.py for building gswc.
-'''
+"""
 
 
 import os
@@ -16,16 +16,16 @@ rootpath = os.path.abspath(os.path.dirname(__file__))
 
 
 def read(*parts):
-    return open(os.path.join(rootpath, *parts), 'r').read()
+    return open(os.path.join(rootpath, *parts), "r").read()
 
 
 class build_ext(_build_ext):
     # Extension builder from pandas without the cython stuff
     def build_extensions(self):
-        numpy_incl = pkg_resources.resource_filename('numpy', 'core/include')
+        numpy_incl = pkg_resources.resource_filename("numpy", "core/include")
 
         for ext in self.extensions:
-            if hasattr(ext, 'include_dirs') and not numpy_incl in ext.include_dirs:
+            if hasattr(ext, "include_dirs") and not numpy_incl in ext.include_dirs:
                 ext.include_dirs.append(numpy_incl)
         _build_ext.build_extensions(self)
 
@@ -33,26 +33,33 @@ class build_ext(_build_ext):
 # MSVC can't handle C complex, and distutils doesn't seem to be able to
 # let us force C++ compilation of .c files, so we use the following hack for
 # Windows.
-if sys.platform == 'win32':
-    cext = 'cpp'
-    shutil.copy('src/c_gsw/gsw_oceanographic_toolbox.c',
-                'src/c_gsw/gsw_oceanographic_toolbox.cpp')
-    shutil.copy('src/c_gsw/gsw_saar.c', 'src/c_gsw/gsw_saar.cpp')
+if sys.platform == "win32":
+    cext = "cpp"
+    shutil.copy(
+        "src/c_gsw/gsw_oceanographic_toolbox.c",
+        "src/c_gsw/gsw_oceanographic_toolbox.cpp",
+    )
+    shutil.copy("src/c_gsw/gsw_saar.c", "src/c_gsw/gsw_saar.cpp")
 else:
-    cext = 'c'
+    cext = "c"
 
-ufunc_src_list = ['src/_ufuncs.c',
-                  'src/c_gsw/gsw_oceanographic_toolbox.' + cext,
-                  'src/c_gsw/gsw_saar.' + cext]
+ufunc_src_list = [
+    "src/_ufuncs.c",
+    "src/c_gsw/gsw_oceanographic_toolbox." + cext,
+    "src/c_gsw/gsw_saar." + cext,
+]
 
 config = {
+    # The package metadata is specified in setup.cfg but GitHub's downstream dependency graph
+    # does not work unless we put the name this here too.
+    "name": "gsw",
     "use_scm_version": {
         "write_to": "gsw/_version.py",
         "write_to_template": '__version__ = "{version}"',
         "tag_regex": r"^(?P<prefix>v)?(?P<version>[^\+]+)(?P<suffix>.*)?$",
     },
-    "ext_modules": [Extension('gsw._gsw_ufuncs', ufunc_src_list)],
-    "include_dirs": [os.path.join(rootpath, 'src', 'c_gsw')],
+    "ext_modules": [Extension("gsw._gsw_ufuncs", ufunc_src_list)],
+    "include_dirs": [os.path.join(rootpath, "src", "c_gsw")],
     "cmdclass": {"build_ext": build_ext},
 }
 
